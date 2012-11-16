3090ac3d7654c7458821e46bd5c7f93a13dd220f
Lukasz Bandzarewicz <lucassus@gmail.com>
2012-11-16 12:34:15 +0100

    Add integration spec for deleting a database.

--- spec/features/databases_list_spec.rb
+++ spec/features/databases_list_spec.rb
@@ -34,4 +34,19 @@ describe "Databases list", type: :request do
     end
   end
 
+  describe "click on delete database button", js: true do
+    it "deletes a database" do
+      click_delete_button_for("second_database")
+
+      within "table.databases" do
+        expect(page).to have_link("first_database")
+        expect(page).to_not have_link("second_database")
+      end
+    end
+
+    def click_delete_button_for(database_name)
+      database_row = find(:xpath, %Q{//table//tr//*[contains(text(), "#{database_name}")]/../..})
+      within(database_row) { click_link "Delete" }
+    end
+  end
 end
