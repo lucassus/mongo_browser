d3b3c9a6976428fbf4b7d074951a1a259aae20e7
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-11-24 17:17:38 +0100

    Attempt for fix travis-ci config for jasmine.

--- spec/javascripts/support/jasmine_runner.rb
+++ spec/javascripts/support/jasmine_runner.rb
@@ -4,11 +4,7 @@ require 'rubygems'
 require 'jasmine'
 jasmine_config_overrides = File.expand_path(File.join(Dir.pwd, 'spec', 'javascripts', 'support', 'jasmine_config.rb'))
 require jasmine_config_overrides if File.exist?(jasmine_config_overrides)
-if Jasmine::Dependencies.rspec2?
-  require 'rspec'
-else
-  require 'spec'
-end
+require 'rspec'
 
 jasmine_runner_config = Jasmine::RunnerConfig.new
 root_path = jasmine_runner_config.project_root
