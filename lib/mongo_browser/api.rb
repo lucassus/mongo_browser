603c48aaf063f15760356ea9f31e989449ca4a06
Lukasz Bandzarewicz <lucassus@gmail.com>
2013-06-11 11:40:04 +0200

    Log api calls, closes #97

--- lib/mongo_browser/api.rb
+++ lib/mongo_browser/api.rb
@@ -6,9 +6,20 @@ module MongoBrowser
     format :json
     rescue_from :all, backtrace: true
 
+    before do
+      method = env["REQUEST_METHOD"]
+      path = env["PATH_INFO"]
+
+      logger.info "[api] #{method} #{path}"
+    end
+
     helpers do
       def server
         @server ||= MongoBrowser::Models::Server.current
+      end
+
+      def logger
+        Grape::API.logger
       end
     end
 
