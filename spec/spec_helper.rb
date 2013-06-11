481b1b944f81a6eb4fd10109bcba319c8451904e
Lukasz Bandzarewicz <lucassus@gmail.com>
2013-06-11 10:28:24 +0200

    Do not delete all database inside the fixtures loader.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,21 +10,8 @@ require "rack/test"
 # from spec/support/ and its subdirectories.
 Dir[File.expand_path("spec/support/**/*.rb")].each { |f| require f }
 
-test_server = Mongod.instance
 fixtures = Fixtures.instance
 
 RSpec.configure do |config|
-  # Run test mongod instance and load database fixtures
-  config.before do
-    test_server.start! do |port|
-      MongoBrowser.mongodb_host = "127.0.0.1"
-      MongoBrowser.mongodb_port = port
-    end
-
-    fixtures.load!
-  end
-end
-
-at_exit do
-  test_server.shutdown!
+  config.before { fixtures.load! }
 end
