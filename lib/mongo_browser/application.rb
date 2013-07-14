262865e6a94bd75ad26a6cb657868faa53a5332e
Lukasz Bandzarewicz <lucassus@gmail.com>
2013-07-14 16:47:28 +0200

    Get rid of sprockets.

--- lib/mongo_browser/application.rb
+++ lib/mongo_browser/application.rb
@@ -2,24 +2,13 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/json"
 
-require "sprockets"
-require "sass"
-require "coffee_script"
-
-require "mongo_browser/middleware/sprockets_sinatra"
-
 module MongoBrowser
   class Application < Sinatra::Base
     set :logging, true
     set :root, File.join(File.dirname(__FILE__), "../../app")
     set :public_folder, Proc.new { File.join(root, "../public") }
 
-    use Middleware::SprocketsSinatra, :root => File.join(settings.root, "..")
-
     if settings.development? or settings.test?
-      require "mongo_browser/middleware/sprockets_specs"
-      use Middleware::SprocketsSpecs, :root => File.join(settings.root, "..")
-
       require "mongo_browser/application/development"
       register Development
     end
--- lib/mongo_browser/application/development.rb
+++ lib/mongo_browser/application/development.rb
@@ -5,18 +5,6 @@ class MongoBrowser::Application
 
       app.register Sinatra::Reloader
 
-      app.set :spec_root, File.join(app.settings.root, "../spec")
-
-      # Execute jasmine runner
-      app.get "/jasmine" do
-        File.read(File.join(app.settings.spec_root, "javascripts/runner.html"))
-      end
-
-      # Execute e2e runner
-      app.get "/e2e" do
-        File.read(File.join(app.settings.spec_root, "javascripts/runner_e2e.html"))
-      end
-
       # Load database fixtures
       app.get "/e2e/load_fixtures" do
         require File.join(app.settings.spec_root, "support/fixtures")
